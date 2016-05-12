3a032edf26318f1123407cb3c357e7fe553b2ff5
natdjerf <nadjerf@gmail.com>
2016-05-12 11:28:59 -0400

    Readfile written.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -5,12 +5,26 @@ const fs = require('fs');
 // set file to command line argument (or temporarily as '' for testing)
 let filename = process.argv[2] || '';
 
-fs.readFile(filename, (err, data) => {
-  if (err) {
-    return console.error(err);
-  }
-  console.log(`${filename} is ${data.length} bytes long.`);
-});
+const readFile = (filename) =>
+  new Promise((resolve, reject) => {
+
+    fs.readFile(filename, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+
+// readFile returns a promise. To get the data read, invoke readFile:
+readFile(filename)
+.then((data) =>
+  console.log(`${filename} is ${data.length} bytes long.`))
+// handle all errors:
+.catch(console.error);
+
+
 
 
   // what is the need for buffer?
