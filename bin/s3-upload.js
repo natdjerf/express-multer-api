a3640fbe7986b9c79cb2917feea5a8f9e1412069
natdjerf <nadjerf@gmail.com>
2016-05-12 11:57:47 -0400

    Install filetype & add parameters
    To detect filetype from buffer.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -17,12 +17,35 @@ const readFile = (filename) =>
     });
   });
 
+// upload to AWS function
+const awsUpload = (data, ) => {
+
+};
+
+
+
+// add necessary paramaters
+const awsS3UploadOptions = {
+  ACL: 'public-read',
+  Body: '',
+  Bucket: 'djerfbucket',
+  ContentType: 'application/octet-stream',
+  Key: 'test/test.bin'
+};
+
+
+
+
 // readFile returns a promise. To get the data read, invoke readFile:
 readFile(filename)
 .then((data) =>
   console.log(`${filename} is ${data.length} bytes long.`))
 // handle all errors:
 .catch(console.error);
+
+
+
+
 
 
 
